b3f47195dce7960a45df040a7a7f35ea01eecf54
Jamie Hannaford <jamie.hannaford@rackspace.com>
2015-07-12 12:47:24 +0200

    finished differentiation stuff

--- HW04.hs
+++ HW04.hs
@@ -1,7 +1,7 @@
 {-# OPTIONS_GHC -Wall #-}
 module HW04 where
 
-import Data.List
+import Data.List (sort, intersect)
 
 newtype Poly a = P [a]
 
@@ -24,44 +24,68 @@ instance (Num a, Eq a, Ord a, Integral a) => Eq (Poly a) where
  
 -- Exercise 3 -----------------------------------------
 
-instance (Num a, Eq a, Show a) => Show (Poly a) where
-    show = undefined
+displayC :: (Num a, Ord a, Eq a, Show a) => a -> a -> String
+displayC c d
+  | c == 0 && d >= 1 = ""
+  | c == 1 && d > 0  = ""
+  | c == -1 && d > 0 = "-"
+  | otherwise        = show c
+
+display :: (Num a, Eq a, Ord a, Show a) => (a, a) -> String
+display (c,d) = (displayC c d) ++ case d of 0 -> ""
+                                            1 -> "x"
+                                            n -> "x^" ++ show n
+
+instance (Num a, Eq a, Show a, Enum a, Ord a) => Show (Poly a) where
+    show (P a) = foldr (\(c,b) st -> st ++ if c /= 0 then (prefix st) ++ display (c,b) else "") "" (zip a [0..])
+      where prefix st = if st /= "" then " + " else ""
 
 -- Exercise 4 -----------------------------------------
 
-plus :: Num a => Poly a -> Poly a -> Poly a
-plus = undefined
+zipWithPadding :: a -> b -> [a] -> [b] -> [(a,b)]
+zipWithPadding a b (z:xs) (y:ys) = (z,y) : zipWithPadding a b xs ys
+zipWithPadding a _ []     ys     = zip (repeat a) ys
+zipWithPadding _ b xs     []     = zip xs (repeat b)
+
+plus :: (Num a) => Poly a -> Poly a -> Poly a
+plus (P a) (P b) = P [m + n | (m,n) <- zipWithPadding 0 0 a b]
 
 -- Exercise 5 -----------------------------------------
 
+pad :: Int -> a -> [a] -> [a]
+pad 0 _ as = as
+pad n a as = a:(pad (n-1) a as)
+
 times :: Num a => Poly a -> Poly a -> Poly a
-times = undefined
+times (P a) (P b) = foldr (+) (P[0]) padded
+  where list   = scanl (\_ val -> map (*val) b) [0] a
+        padded = [P (pad n 0 m) | (m,n) <- zip (tail list) [0..]]
 
 -- Exercise 6 -----------------------------------------
 
 instance Num a => Num (Poly a) where
     (+) = plus
     (*) = times
-    negate      = undefined
-    fromInteger = undefined
+    negate      = times (P[-1])
+    fromInteger = \y -> P [(fromInteger y)]
     -- No meaningful definitions exist
     abs    = undefined
     signum = undefined
 
 -- Exercise 7 -----------------------------------------
 
-applyP :: Num a => Poly a -> a -> a
-applyP = undefined
+applyP :: (Enum a, Floating a, Num a) => Poly a -> a -> a
+applyP (P list) val = foldr (\(v,i) prev -> prev + (v * (val ** i)) ) 0 $ zip list [0..]
 
 -- Exercise 8 -----------------------------------------
 
 class Num a => Differentiable a where
     deriv  :: a -> a
     nderiv :: Int -> a -> a
-    nderiv = undefined
+    nderiv a b = foldl (\acc _ -> deriv acc) b [1..a]
 
 -- Exercise 9 -----------------------------------------
 
-instance Num a => Differentiable (Poly a) where
-    deriv = undefined
+instance (Enum a, Num a) => Differentiable (Poly a) where
+    deriv (P a) = P $ tail $ map (\(val,pos) -> val * pos) $ zip a [0..]
 
